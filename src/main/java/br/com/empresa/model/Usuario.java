ee60926c620f399ac4bc91cc89a2a16d4593253c
thalesgmartins <thalesdiniz30@gmail.com>
2025-12-08 21:04:27 -0300

    add marcações que faltavam na classe do usuario

--- src/main/java/br/com/empresa/model/Usuario.java
+++ src/main/java/br/com/empresa/model/Usuario.java
@@ -10,14 +10,29 @@ import java.util.Date;
 @Entity
 @Table(name = "usuarios")
 public class Usuario extends PanacheEntity {
+
     @Column(name = "nome_completo", length = 300)
     public String nomeUsuario;
+
+    @Column(length = 500)
     public String endereco;
+
+    @Column(length = 100)
     public String cidade;
+
+    @Column(length = 2)
     public String uf;
+
+    @Column(length = 8)
     public String cep;
-    public boolean ativo;
+
+    @Column(length = 150)
     public String email;
+
     @Column(name = "data_cadastro")
     public Date dataCadastro;
+
+    public boolean ativo;
+    public String senha;
+
 }
